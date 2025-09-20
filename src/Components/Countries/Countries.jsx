fbee2f55ed0b4d39da282b3d12f91ddc11628452
ahmed-1430 <ahmedtexting@gmail.com>
2025-09-20 23:19:54 +0600

    style added to country name box

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,17 +1,21 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
     const countriesData = use(countriesPromise)
     const countries = countriesData.countries
     // console.log(countries);
     return (
         <div>
-            <h1>Total Countries: {countries.length}</h1>{
-                countries.map(country => <Country country={country} ></Country>)
-            }
-            
-            
+            <h1>Total Countries: {countries.length}</h1>
+            <div className='countries'>
+                {
+                    countries.map(country =>
+                        <Country key={country.cca3.cca3} country={country} ></Country>)
+                }
+            </div>
+
+
         </div>
     );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import './country.css'
 
-const Country = ({country}) => {
-    console.log(country.name.common)
+const Country = ({ country }) => {
+    // console.log(country.name.common)
     return (
-        <div>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <h2>Name: {country.name.common} </h2>
+        <div className='country'>
+            <div>
+                <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            </div>
+            <div>
+                <h2>Name: {country.name.common} </h2>
+                <h4>Region: {country.region.region} </h4>
+                <p>Population: {country.population.population} </p>
+                <p>Capital City: {country.capital.capital} </p>
+            </div>
         </div>
     );
 };
